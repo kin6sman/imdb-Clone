07874aee84c2f53657c2d067fdc30b5963cc3eb3
kin6sman <domin12313xyz@gmail.com>
2023-03-01 02:46:25 +0530

    L

--- js/movie.js
+++ js/movie.js
@@ -1,40 +1,46 @@
 // Titles: https://www.omdbapi.com/?s=thor&page=1&apikey=bfd6b563
 // details: http://www.omdbapi.com/?i=tt3896198&apikey=bfd6b563
-const movieSearchBox = document.getElementById('movie-search-box');
-const searchList = document.getElementById('search-list');
-const resultGrid = document.getElementById('result-grid');
+const movieSearchBox = document.getElementById("movie-search-box");
+const searchList = document.getElementById("search-list");
+const resultGrid = document.getElementById("result-grid");
+
+let favList = [];
+// let tempList = [];
 
 // load movies from API
-async function loadMovies(searchTerm){
-    const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=bfd6b563`;
-    const res = await fetch(`${URL}`);
-    const data = await res.json();
-    console.log(data.Search);
-    if(data.Response == "True") displayMovieList(data.Search);
+async function loadMovies(searchTerm) {
+  const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=bfd6b563`;
+  const res = await fetch(`${URL}`);
+  const data = await res.json();
+  // console.log(data.Search);
+  if (data.Response == "True") displayMovieList(data.Search);
 }
 
-function findMovies(){
-    let searchTerm = (movieSearchBox.value).trim();
-    if(searchTerm.length > 0){
-        searchList.classList.remove('hide-search-list');
-        loadMovies(searchTerm);
-    } else {
-        searchList.classList.add('hide-search-list');
-    }
+
+
+
+
+
+function findMovies() {
+  let searchTerm = movieSearchBox.value.trim();
+  if (searchTerm.length > 0) {
+    searchList.classList.remove("hide-search-list");
+    loadMovies(searchTerm);
+  } else {
+    searchList.classList.add("hide-search-list");
+  }
 }
 
-function displayMovieList(movies){
-    searchList.innerHTML = "";
-    for(let idx = 0; idx < movies.length; idx++){
-        let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
-        movieListItem.classList.add('search-list-item');
-        if(movies[idx].Poster != "N/A")
-            moviePoster = movies[idx].Poster;
-        else 
-            moviePoster = "imagenotfound.jpg";
+function displayMovieList(movies) {
+  searchList.innerHTML = "";
+  for (let idx = 0; idx < movies.length; idx++) {
+    let movieListItem = document.createElement("div");
+    movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+    movieListItem.classList.add("search-list-item");
+    if (movies[idx].Poster != "N/A") moviePoster = movies[idx].Poster;
+    else moviePoster = "imagenotfound.jpg";
 
-        movieListItem.innerHTML = `
+    movieListItem.innerHTML = `
         <div class = "search-item-thumbnail">
             <img src = "${moviePoster}">
         </div>
@@ -43,31 +49,38 @@ function displayMovieList(movies){
             <p>${movies[idx].Year}</p>
         </div>
         `;
-        searchList.appendChild(movieListItem);
-    }
-    loadMovieDetails();
+    searchList.appendChild(movieListItem);
+  }
+  loadMovieDetails();
 }
 
-function loadMovieDetails(){
-    const searchListMovies = searchList.querySelectorAll('.search-list-item');
-    searchListMovies.forEach(movie => {
-        movie.addEventListener('click', async () => {
-            // console.log(movie.dataset.id);
-            searchList.classList.add('hide-search-list');
-            movieSearchBox.value = "";
-            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=bfd6b563`);
-            const movieDetails = await result.json();
-            // console.log(movieDetails);
-            displayMovieDetails(movieDetails);
-        });
+function loadMovieDetails() {
+  const searchListMovies = searchList.querySelectorAll(".search-list-item");
+  searchListMovies.forEach((movie) => {
+    movie.addEventListener("click", async () => {
+      // console.log(movie.dataset.id);
+      searchList.classList.add("hide-search-list");
+      movieSearchBox.value = "";
+      const result = await fetch(
+        `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=bfd6b563`
+      );
+      const movieDetails = await result.json();
+      // console.log(movieDetails);
+      displayMovieDetails(movieDetails);
     });
+  });
 }
 
-function displayMovieDetails(details){
+function displayMovieDetails(details) {
+  console.log(details);
+
   document.getElementById("overlay").style.display = "block";
-    resultGrid.innerHTML = `
+
+  resultGrid.innerHTML = `
     <div class = "movie-poster">
-        <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
+        <img src = "${
+          details.Poster != "N/A" ? details.Poster : "image_not_found.png"
+        }" alt = "movie poster">
     </div>
     <div class = "movie-info">
         <h3 class = "movie-title">${details.Title}</h3>
@@ -81,21 +94,44 @@ function displayMovieDetails(details){
         <p class = "actors"><b>Actors: </b>${details.Actors}</p>
         <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
         <p class = "language"><b>Language:</b> ${details.Language}</p>
-        <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
+        <p class = "awards"><b><i class = "fas fa-award"></i></b> ${
+          details.Awards
+        }</p>
+        <button id="fav-btn" >fav</button>
     </div>
     `;
+
+  const fav_btn = document.getElementById("fav-btn");
+  // tempList.push(details);
+  fav_btn.addEventListener("click", () => {
+    favList.push(details.imdbID);
+    console.log("favlist: " + favList);
+  });
 }
 
-
-window.addEventListener('click', (event) => {
-    if(event.target.className != "form-control"){
-        searchList.classList.add('hide-search-list');
-    }
+window.addEventListener("click", (event) => {
+  if (event.target.className != "form-control") {
+    searchList.classList.add("hide-search-list");
+  }
 });
-
-
-
 
 function off() {
   document.getElementById("overlay").style.display = "none";
+}
+
+
+//load movies to fav section
+
+favList.forEach((id)=> {
+  console.log(id + "helli");
+  sum += id;
+});
+
+document.getElementById("demo").innerHTML = sum;
+
+function reload(){
+  favList.forEach((id)=> {
+    
+  });
+  
 }
